06e903445b31708c40c323b1b189177b08563ebc
trade-hw <102856235+trade-hw@users.noreply.github.com>
2022-04-11 15:28:09 +0900

    Update autoTradeMain.py

--- autoTradeMain.py
+++ autoTradeMain.py
@@ -42,7 +42,7 @@ def get_balance(upbit,coin):
 
 def get_current_price(ticker):
     """현재가 조회"""
-    return pyupbit.get_orderbook(tickers=ticker)[0]["orderbook_units"][0]["ask_price"]
+    return pyupbit.get_orderbook(ticker=ticker)["orderbook_units"][0]["ask_price"]
 
 def StartAutoTrade(coin):
     
